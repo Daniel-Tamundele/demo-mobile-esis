e92551285462715da83a28e737c02c5c644d9e4c
Yves Kalume <55670723+YvesKalume@users.noreply.github.com>
2022-07-05 17:22:16 +0200

    initial commit (#2)

--- app/src/main/java/org/esissalama/mobile/demo/MainActivity.java
+++ app/src/main/java/org/esissalama/mobile/demo/MainActivity.java
@@ -1,14 +1,53 @@
 package org.esissalama.mobile.demo;
 
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.Button;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    int[] dices = {
+            R.drawable.dice_1,
+            R.drawable.dice_2,
+            R.drawable.dice_3,
+            R.drawable.dice_4,
+            R.drawable.dice_5,
+            R.drawable.dice_6
+    };
+
+    ImageView image = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        image = findViewById(R.id.ivDes);
+
+        button.setOnClickListener(v -> {
+
+            new CountDownTimer(1500,1) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    roll();
+                }
+
+                @Override
+                public void onFinish() {
+                    roll();
+                }
+            }.start();
+        });
+    }
+
+    private void roll() {
+        int indice = new Random().nextInt(6);
+        image.setImageResource(dices[indice]);
     }
 }
